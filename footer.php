cc9d747b9f00374ed8b1ea59a1cdb3456f8f175c
sakunasanka <sakunasanka@gmail.com>
2024-09-01 16:13:13 +0530

    Create footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="footer-container">
+        <p>&copy; 2024 UniQuest. All rights reserved.</p>
+        <div class="footer-links">
+            <a href="#">Terms of Services</a>
+            <a href="#">Privacy Policy</a>
+        </div>
+    </div>
+</footer>
