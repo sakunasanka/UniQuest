d0c661fa7959af83092bfe1310eb15a776cec94f
sakiththewmika <sakiththewmika@gmail.com>
2024-09-03 23:20:44 +0530

    [UQMVP-3] Implement admin side panel with basic styles

---
+++ views/components/adminSidePanel.php
@@ -0,0 +1,28 @@
+<aside class="sidebar">
+    <nav class="nav-menu">
+        <a href="#">
+            <button class="nav-btn active">Dashboard</button>
+        </a>
+        <a href="#">
+            <button class="nav-btn">User Management</button>
+        </a>
+        <a href="#">
+            <button class="nav-btn">Jobs Management</button>
+        </a>
+        <a href="#">
+            <button class="nav-btn">User Verification</button>
+        </a>
+        <a href="#">
+            <button class="nav-btn">Jobs Verification</button>
+        </a>
+        <a href="#">
+            <button class="nav-btn">Complaints</button>
+        </a>
+        <a href="#">
+            <button class="nav-btn">Analytics</button>
+        </a>
+        <a href="#">
+            <button class="nav-btn">Reports</button>
+        </a>
+    </nav>
+</aside>
