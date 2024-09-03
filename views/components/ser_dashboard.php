a748702f274e2701f97d587a5d585b116abf026d
sakunasanka <sakunasanka@gmail.com>
2024-09-04 01:27:37 +0530

    [UQMVP-16] Update dashboard

--- views/components/ser_dashboard.php
+++ views/components/ser_dashboard.php
@@ -6,15 +6,18 @@
     <title>UniQuest Dashboard</title>
     <link rel="stylesheet" href="../../assets/css/components/ser_dashboard.css">
     <link rel="stylesheet" href="../../assets/css/components/styles.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
 </head>
 <body>
     <main class="dashboard">
-        <h1>Welcome!</h1>
+        <div class="header-section">
+            <h1>Welcome!</h1>
+            <button class="activate-btn" id="activate-premium">
+                <img src="../../assets/icons/premium.png" alt="Activate Premium"> Activate Premium
+            </button>
+        </div>
         <div class="grid-container">
             <div class="card">
-                <i class="icon-jobs"></i>
+                <img src="../../assets/icons/suitcase.png" alt="Jobs Icon" class="icon">
                 <h2>Jobs</h2>
                 <p>View, Edit and manage your jobs.</p>
                 <div class="card-stats">
@@ -25,17 +28,23 @@
             </div>
 
             <div class="card">
-                <i class="icon-applications"></i>
+                <img src="../../assets/icons/application.png" alt="Applications Icon" class="icon">
                 <h2>Applications</h2>
                 <p>View applications for your jobs.</p>
-                <div class="card-stats">
-                    <span>3 New</span>
-                    <span>4 Active</span>
+                <div class="applications-stats">
+                    <div class="new">
+                        <span class="new-stat">3</span>
+                        <span class="new-label">New</span>
+                    </div>
+                    <div class="active">
+                        <span class="active-stat">4</span>
+                        <span class="active-label">Active</span>
+                    </div>  
                 </div>
             </div>
 
             <div class="card">
-                <i class="icon-analytics"></i>
+                <img src="../../assets/icons/analytics.png" alt="Analytics Icon" class="icon">
                 <h2>Analytics</h2>
                 <p>View analytics and generate reports related to jobs.</p>
                 <div class="card-stats">
@@ -46,7 +55,7 @@
             </div>
 
             <div class="card">
-                <i class="icon-reviews"></i>
+                <img src="../../assets/icons/review.png" alt="Community Reviews Icon" class="icon">
                 <h2>Community Reviews</h2>
                 <p>View, respond to, and manage your reviews.</p>
                 <div class="card-stats">
@@ -58,8 +67,5 @@
         </div>
     </main>
 
-    <script src="scripts.js"></script>
 </body>
 </html>
-
-
