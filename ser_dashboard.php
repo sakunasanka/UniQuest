1c8ac7e054a40d85048d93af04ae9ac0a6ac32be
sakunasanka <sakunasanka@gmail.com>
2024-09-03 23:19:10 +0530

    [UQMVP-16] Create service provider dashboard structure

---
+++ ser_dashboard.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UniQuest Dashboard</title>
+    <link rel="stylesheet" href="ser_dashboard.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+</head>
+<body>
+    <main class="dashboard">
+        <h1>Welcome!</h1>
+        <div class="grid-container">
+            <div class="card">
+                <i class="icon-jobs"></i>
+                <h2>Jobs</h2>
+                <p>View, Edit and manage your jobs.</p>
+                <div class="card-stats">
+                    <span>8</span>
+                    <span class="down">▼ 10.5%</span>
+                </div>
+                <p class="card-subtext">Job Clicks (Last month)</p>
+            </div>
+
+            <div class="card">
+                <i class="icon-applications"></i>
+                <h2>Applications</h2>
+                <p>View applications for your jobs.</p>
+                <div class="card-stats">
+                    <span>3 New</span>
+                    <span>4 Active</span>
+                </div>
+            </div>
+
+            <div class="card">
+                <i class="icon-analytics"></i>
+                <h2>Analytics</h2>
+                <p>View analytics and generate reports related to jobs.</p>
+                <div class="card-stats">
+                    <span>4.2</span>
+                    <span class="up">▲ 2.6%</span>
+                </div>
+                <p class="card-subtext">Company Rating (Last month)</p>
+            </div>
+
+            <div class="card">
+                <i class="icon-reviews"></i>
+                <h2>Community Reviews</h2>
+                <p>View, respond to, and manage your reviews.</p>
+                <div class="card-stats">
+                    <span>4</span>
+                    <span class="up">▲ 3.5%</span>
+                </div>
+                <p class="card-subtext">Company Reviews (Last month)</p>
+            </div>
+        </div>
+    </main>
+
+    <script src="scripts.js"></script>
+</body>
+</html>
+
+
