347c4e8ea8bf3389d8b8e7da389edb6db86f3c79
sakiththewmika <sakiththewmika@gmail.com>
2024-09-10 19:55:38 +0530

    [UQMVP-3] Edit data paths and js

--- app/views/components/adminSidePanel.php
+++ app/views/components/adminSidePanel.php
@@ -1,21 +1,38 @@
 <aside class="sidebar">
     <nav class="nav-menu">
+        <button class="nav-btn" data-path="/uniquest/admin/dashboard">
+            Dashboard
+        </button>
 
-        <button class="nav-btn" data-path="/uniquest/admin/dashboard">Dashboard</button>
+        <button class="nav-btn" data-paths="/uniquest/admin/students_mng,/uniquest/admin/company_mng,/uniquest/admin/verTeam_mng">
+            User Management
+        </button>
 
-        <button class="nav-btn" data-path="/uniquest/admin/students_mng">User Management</button>
+        <button class="nav-btn" data-paths="/uniquest/admin/ptjobs_mng,/uniquest/admin/intern_mng">
+            Jobs Management
+        </button>
 
-        <button class="nav-btn" data-path="/uniquest/admin/ptjobs_mng">Jobs Management</button>
+        <button class="nav-btn" data-paths="/uniquest/admin/user_ver_all,/uniquest/admin/user_ver_pending,/uniquest/admin/user_ver_not">
+            User Verification
+        </button>
 
-        <button class="nav-btn" data-path="/uniquest/admin/user_ver">User Verification</button>
+        <button class="nav-btn" data-paths="/uniquest/admin/job_ver_all,/uniquest/admin/job_ver_pending,/uniquest/admin/job_ver_not">
+            Jobs Verification
+        </button>
 
-        <button class="nav-btn" data-path="/uniquest/admin/job_ver">Jobs Verification</button>
+        <button class="nav-btn" data-paths="/uniquest/admin/job_complaint,/uniquest/admin/company_complaint">
+            Complaints
+        </button>
 
-        <button class="nav-btn" data-path="/uniquest/admin/job_complaint">Complaints</button>
+        <button class="nav-btn" data-path="/uniquest/admin/analytics">
+            Analytics
+        </button>
 
-        <button class="nav-btn" data-path="/uniquest/admin/analytics">Analytics</button>
-
-        <button class="nav-btn" data-path="/uniquest/admin/reports">Reports</button>
+        <button class="nav-btn" data-path="/uniquest/admin/reports">
+            Reports
+        </button>
 
     </nav>
 </aside>
+
+<script type="module" src="<?php echo URLROOT; ?>/public/js/components/adminSidePanel.js"></script>
