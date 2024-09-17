2d6475a6110b653f29497aaeacdf6da67fbf1e65
sakunasanka <sakunasanka@gmail.com>
2024-09-17 15:22:51 +0530

    [UQMVP-20] Create premium features page

--- app/controllers/service_provider.php
+++ app/controllers/service_provider.php
@@ -16,4 +16,9 @@ class Service_provider extends Controller
         $this->view('pages/service_provider/contact_admin');
     }
 
+    public function premium()
+    {
+        $this->view('pages/service_provider/premiumFeatures');
+    }
+
 }
