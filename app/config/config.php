663efd5beb40ef82d82b9369360deed95b9811c4
sakiththewmika <sakiththewmika@gmail.com>
2024-09-20 01:20:23 +0530

    [UQMVP-43] Implement database connection

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,10 @@
 <?php 
+    //DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'uniquest');
+    
     //App Root
     define('APPROOT', dirname(dirname(__FILE__)));
 
