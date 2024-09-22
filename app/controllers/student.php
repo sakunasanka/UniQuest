6a1551b10b26378de609fba7a75793cd3ffe91b4
Wameesha <rasanjani9jayasingha@gmail.com>
2024-09-22 12:37:49 +0530

    [UQMVP-40] Create jobs description page

--- app/controllers/student.php
+++ app/controllers/student.php
@@ -25,5 +25,10 @@ class Student extends Controller
     {
         $this->view('pages/student/students_mng');
     }
+
+    public function jobsDescription()
+    {
+        $this->view('pages/student/jobsDescription');
+    }
 }
 
