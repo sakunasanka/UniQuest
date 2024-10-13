711d0e75e1ac0c6bc810b68b4382f63faa2ceab3
sakiththewmika <sakiththewmika@gmail.com>
2024-10-13 23:06:42 +0530

    [UQMVP-26] Change some styles

--- app/views/components/verificationTeamSidePanel.php
+++ app/views/components/verificationTeamSidePanel.php
@@ -1,18 +1,22 @@
 <aside class="sidebar">
     <nav class="nav-menu">
         <button class="nav-btn" data-path="/uniquest/verification_team/profile">
+        <span class="material-symbols-outlined"> person </span>
             Profile
         </button>
 
         <button class="nav-btn" data-path="/uniquest/verification_team/dashboard">
+        <span class="material-symbols-outlined"> dashboard </span>
             Dashboard
         </button>
 
         <button class="nav-btn" data-paths="/uniquest/verification_team/user_ver_all,/uniquest/verification_team/user_ver_pending,/uniquest/verification_team/user_ver_not">
+        <span class="material-symbols-outlined"> person_check </span>
             User Verification
         </button>
 
         <button class="nav-btn" data-paths="/uniquest/verification_team/job_ver_all,/uniquest/verification_team/job_ver_pending,/uniquest/verification_team/job_ver_not">
+        <span class="material-symbols-outlined"> domain_verification </span>
             Jobs Verification
         </button>
 
