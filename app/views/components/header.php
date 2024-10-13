83745914f43fca633a5a05451eb95c4a2cf78808
sakunasanka <sakunasanka@gmail.com>
2024-10-14 01:13:07 +0530

    [UQMVP-33] Edit navbar

--- app/views/components/header.php
+++ app/views/components/header.php
@@ -1,20 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo SITENAME ?></title>
-
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/styles.css">
-    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-
-</head>
-
-<body>
-
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -34,5 +19,3 @@
             </div>
         </div>
     </nav>
-</body>
-</html>
