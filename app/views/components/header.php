0b19c1ab5c9bb5a0b6e2728fc0cac590942e177a
sakunasanka <sakunasanka@gmail.com>
2024-10-14 23:48:19 +0530

    [dev] Corrected data paths

--- app/views/components/header.php
+++ app/views/components/header.php
@@ -16,7 +16,7 @@
                 <img src="<?php echo URLROOT; ?>/images/UniQuest3.png" alt="UniQuest Logo">
             </div>
             <div class="nav-button">
-                <a href="#" class="get-started">Get Started</a>
+                <a href="/uniquest/student/login" class="get-started">Get Started</a>
             </div>
         </div>
     </nav>
