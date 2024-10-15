ae75c6e6e4a6196475dfe29258fee8291930923a
sakiththewmika <sakiththewmika@gmail.com>
2024-10-15 15:00:01 +0530

    [UQMVP-2] Change some styles

--- app/views/pages/service_provider/offered_applications.php
+++ app/views/pages/service_provider/offered_applications.php
@@ -1,7 +1,5 @@
 <?php require APPROOT . '/views/components/ser_header.php'; ?>
 
-<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/pages/service_provider/jobs_table.css">
-
 <!-- Sidebar and Content Layout -->
 <div class="main-container">
     <!-- Sidebar -->
@@ -9,18 +7,14 @@
 
     <!-- Content Area -->
     <main class="content-area">
-        <div class="tabs-header">
-            <h1>Offered Applications</h1>
-            <!-- <button class="tab" style="border-radius: 10px 0px 0px 10px;" data-path="/uniquest/admin/students_mng">Students</button>
-            <button class="tab" data-path="/uniquest/admin/company_mng">Companies</button>
-            <button class="tab" style="border-radius: 0px 10px 10px 0px;" data-path="/uniquest/admin/verTeam_mng">Verification Team</button> -->
+    <div class="tabs-header">
+            <button class="tab" style="border-radius: 10px 0px 0px 10px;" data-path="/uniquest/service_provider/new_applications">New Applications</button>
+            <button class="tab" data-path="/uniquest/service_provider/offered_applications">Offered Applications</button>
+            <button class="tab" style="border-radius: 0px 10px 10px 0px;" data-path="/uniquest/service_provider/rejected_applications">Rejected Applications</button>
         </div>
         <div class="table-block">
             <div class="content-header">
-                <div class="input-container">
-                    <span class="material-symbols-outlined icon">search</span>
-                    <input type="text" class="search" placeholder="Search for Job Titles...">
-                </div>
+                <?php require APPROOT . '/views/components/tableSearchBar.php'; ?>
             </div>
             <table>
                 <thead>
@@ -85,20 +79,12 @@
                     </tr>
                 </tbody>
             </table>
-            <div class="pagination">
-                <button class="page-btn prev">&laquo;</button>
-                <button class="page-btn active">1</button>
-                <button class="page-btn">2</button>
-                <button class="page-btn">3</button>
-                <button class="page-btn next">&raquo;</button>
-            </div>
+            <?php require APPROOT . '/views/components/pagination.php'; ?>
         </div>
     </main>
 </div>
 
 <script type="module" src="<?php echo URLROOT; ?>/public/js/components/adminTopPanel.js"></script>
-<script type="module" src="<?php echo URLROOT; ?>/public/js/components/adminPagination.js"></script>
-<script type="module" src="<?php echo URLROOT; ?>/public/js/components/adminSearchNames.js"></script>
 <script type="module" src="<?php echo URLROOT; ?>/public/js/components/adminSortTable.js"></script>
 
 <?php require APPROOT . '/views/components/footer.php'; ?>
