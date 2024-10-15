3a27e990e6a7e6395afb849e405847a707e5c428
sakiththewmika <sakiththewmika@gmail.com>
2024-10-15 22:09:36 +0530

    [UQMVP-44] Implement add student and add company

--- app/views/pages/admin/add_company.php
+++ app/views/pages/admin/add_company.php
@@ -11,33 +11,54 @@
             <div class="content-header">
                 <button class="back-btn">
                     <span class="material-symbols-outlined">arrow_back_ios</span>
-                    <h1>Verification Team Management</h1>
+                    <h1>Company Management</h1>
                 </button>
 
             </div>
             <form class="add-member-form">
-                <h2>Personal Details</h2>
+                <h2>Company Details</h2>
                 <div class="form-row">
                     <div class="input-container">
-                        <label for="firstName">First Name</label>
-                        <input type="text" id="firstName" name="firstName" placeholder="Enter First Name" required>
+                        <label for="name">Company Name</label>
+                        <input type="text" id="name" name="name" placeholder="Enter Company Name" required>
                     </div>
                     <div class="input-container">
-                        <label for="lastName">Last Name</label>
-                        <input type="text" id="lastName" name="lastName" placeholder="Enter Last Name" required>
+                        <label for="logo">Company Logo</label>
+                        <input type="file" id="logo" name="logo">
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="input-container">
-                        <label for="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="Enter Email" required>
+                        <label for="email">Company Email</label>
+                        <input type="email" id="email" name="email" placeholder="Enter Company Email" required>
                     </div>
                     <div class="input-container">
-                        <label for="mobile">Mobile Number</label>
-                        <input type="text" id="mobile" name="mobile" placeholder="Enter Mobile Number" required>
+                        <label for="phone">Contact Number</label>
+                        <input type="text" id="phone" name="phone" placeholder="Enter Contact Number" required>
                     </div>
                 </div>
-                <h2>Login Credentials</h2>
+                <h3>Address</h3>
+                <div class="form-row">
+                    <div class="input-container">
+                        <label for="address-no">Street Number</label>
+                        <input type="text" id="address-no" name="address-no" placeholder="Enter Street Number" required>
+                    </div>
+                    <div class="input-container">
+                        <label for="address-line1">Address Line 1</label>
+                        <input type="text" id="address-line1" name="address-line1" placeholder="Enter Address Line 1" required>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="input-container">
+                        <label for="address-line2">Address Line 2</label>
+                        <input type="text" id="address-line2" name="address-line2" placeholder="Enter Address Line 2">
+                    </div>
+                    <div class="input-container">
+                        <label for="city">City</label>
+                        <input type="text" id="city" name="city" placeholder="Enter City" required>
+                    </div>
+                </div>
+                <h2>Login Credential</h2>
                 <div class="form-row">
                     <div class="input-container">
                         <label for="userName">User Name</label>
@@ -56,7 +77,7 @@
                     </div>
                 </div>
                 <div class="form-row">
-                <div class="input-container"></div>
+                    <div class="input-container"></div>
                     <button class="add-btn" type="submit">
                         <span class="material-symbols-outlined">save</span>
                         <span class="add-btn-text">Save</span>
