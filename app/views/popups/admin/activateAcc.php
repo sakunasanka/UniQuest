19bb333aae3904954b2c2a40a53cc440e1ad2e16
sakunasanka <sakunasanka@gmail.com>
2024-11-02 21:39:08 +0530

    [UQMVP-14] Create test popup

---
+++ app/views/popups/admin/activateAcc.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/popups/admin/activateAcc.css">
+<div class="popup-container">
+<div class="popup" id="popup-1">
+    <div class="overlay"></div>
+    <div class="content">
+        <div class="close-btn" onclick="togglePopup()">&times;</div>
+        <h1>Activate Account</h1>
+        <p>Are you sure you want to activate this account?</p>
+    </div>
+</div>
+
+<button onclick="togglePopup()">Show Popup</button>
+</div>
+
+<script src="<?php echo URLROOT; ?>/public/js/admin/popups.js"></script>
+
