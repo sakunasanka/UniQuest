c9a6469101251c482db28ce5d5ab899a33ce550f
sakunasanka <sakunasanka@gmail.com>
2024-11-04 13:30:59 +0530

    [UQMVP-39] Corrected alignments

--- app/views/pages/student/contact_sp.php
+++ app/views/pages/student/contact_sp.php
@@ -1,35 +1,35 @@
 <?php require APPROOT . '/views/components/stu_header.php'; ?>
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/pages/student/contact_sp.css">
 
-<header class="header">
-
-</header>
-
-<body>
+<div class="main-container">
 <?php require APPROOT . '/views/components/studentSidePanel.php'; ?>
 
-    <div class="contact-container">
-        <div class="contact-left">
-            <h1>Contact Service Provider</h1>
-            <p>Fill out the form below to reach out about jobs, internships, or other opportunities</p>
-            <form class="contactForm">
-                <label for="name">Name:</label>
-                <input type="text" placeholder="Enter your Name" required>
+    <div class="content-area">
+        <div class="container">
+            <div class="contact-left">
+                <h1>Contact Service Provider</h1>
+                <p>Fill out the form below to reach out about jobs, internships, or other opportunities</p>
+                <form class="contactForm">
+                    <label for="name">Name:</label>
+                    <input type="text" placeholder="Enter your Name" required>
 
-                <label for="email">Email:</label>
-                <input type="email" placeholder="Email" required>
-                <span class="error-message" id="emailError"></span>
+                    <label for="email">Email:</label>
+                    <input type="email" placeholder="Email" required>
+                    <span class="error-message" id="emailError"></span>
 
-                <label for="message">Message:</label>
-                <textarea placeholder="Message" required></textarea>
+                    <label for="message">Message:</label>
+                    <textarea placeholder="Message" required></textarea>
 
-                <button type="submit">Send</button>
-            </form>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+            <div class="contact-right">
+                <img src="<?php echo URLROOT; ?>/images/Contact-us.png" alt="Contact Us Image">
+            </div>
         </div>
-        <div class="contact-right">
-            <img src="<?php echo URLROOT; ?>/images/Contact-us.png" alt="Contact Us Image">
-        </div>
+        
     </div>
-</body>
+</div>
 
 <script>
 document.getElementById("contactForm").addEventListener("submit", function(event) {
