ca213acf7795bc6ec1f7ca71a8d31951becea224
shehara Gamage <140165643+sheharagamage@users.noreply.github.com>
2024-11-10 19:42:42 +0530

    update student_delete_ popup

--- public/js/student/delete_account.js
+++ public/js/student/delete_account.js
@@ -1,19 +1,22 @@
-let dapopup = document.getElementById("popup-stu");
 
-function showdeleteaccountconfirm() {
-    popup_stu.classList.toggle("active");
-}
-
-let dapopup =document.querySelector(".container");
+let dapopup =document.querySelector(".l-container");
 const pageWrapper = document.querySelector(".page-wrapper");
 function showdeleteaccountconfirm(){
+    
+    
     dapopup.classList.add("active");
+    pageWrapper.classList.add("blur");
+    
+
 }
 
 function closedeleteaccountconfirm(){
     dapopup.classList.remove("active");
+    pageWrapper.classList.remove("blur");
+
 }
 
 function canceldeleteaccountconfirm(){
     dapopup.classList.remove("active");
+    pageWrapper.classList.remove("blur");
 }
