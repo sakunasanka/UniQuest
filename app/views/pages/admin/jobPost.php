60997d5d7e594614ef568e5a133ea8cf703d4990
shehara Gamage <140165643+sheharagamage@users.noreply.github.com>
2024-11-12 00:52:48 +0530

    update jobpost.php

--- app/views/pages/admin/jobPost.php
+++ app/views/pages/admin/jobPost.php
@@ -8,31 +8,38 @@
             <div class="form-left">
                 <label for="jobName">Job Name:</label>
                 <input type="text" id="jobName" name="jobName" placeholder="Job Name" required  value="<?php echo $data['job_name']; ?>">
+                <span class="form-invalid"><?php echo $data['job_name_err']; ?></span>
 
                 <label for="jobDescription">Job Description:</label>
                 <textarea id="jobDescription" name="jobDescription" placeholder="Job Description" required value="<?php echo $data['Description']; ?>"></textarea>
+                <span class="form-invalid"><?php echo $data['Description_err']; ?></span>
 
                 <label for="jobBenefits">Job Benefits:</label>
                 <input type="text" id="jobBenefits" name="jobBenefits" placeholder="Job Benefits" value="<?php echo $data['job_benifits']; ?>">
+                <span class="form-invalid"><?php echo $data['job_benifits_err']; ?></span>
 
                 <label for="jobLocation">Job Location:</label>
                 <input type="text" id="jobLocation" name="jobLocation" placeholder="Job Location" value="<?php echo $data['job_location']; ?>">
+                <span class="form-invalid"><?php echo $data['job_location_err']; ?></span>
 
                 <label for="salaryRange">Salary Range:</label>
                 <input type="text" id="salaryRange" name="salaryRange" placeholder="Salary Range" value="<?php echo $data['salary_range']; ?>">
+                <span class="form-invalid"><?php echo $data['salary_range_err']; ?></span>
             </div>
 
             <div class="form-right">
                 <label for="qualifications">Required Qualifications:</label>
                 <input type="text" id="qualifications" name="qualifications" placeholder="Required Qualifications" value="<?php echo $data['required_skills']; ?>">
+                <span class="form-invalid"><?php echo $data['required_skills_err']; ?></span>
 
                 <label for="address">Address:</label>
                 <textarea id="address" name="address" placeholder="Address" value="<?php echo $data['adress']; ?>"></textarea>
+                <span class="form-invalid"><?php echo $data['adress_err']; ?></span>
 
                 <label for="jobType">Job Type:</label>
                 <select id="jobType" name="jobType" required>
                     <option value="Full-time">Full-time</option>
-                    <option value="Part-time">Part-time</option>
+                    <optivon value="Part-time">Part-time</optivon>
                     <option value="Internship">Internship</option>
                 </select>
 
