825e9874fa388f0b62c68599f422edfad57b9f4a
shehara Gamage <140165643+sheharagamage@users.noreply.github.com>
2024-11-12 13:21:50 +0530

    update jobpost.php

--- app/models/M_jobpost.php
+++ app/models/M_jobpost.php
@@ -7,23 +7,25 @@
         }
         public function create($data){
             $this->db->query('INSERT INTO jobs (Title, Description, Location, Category, JobBenefits, Address, RequiredQualifications, SalaryRange, CompanyID) VALUES (:job_name, :Description, :job_location, :job_category, :job_benifits, :adress, :required_skills, :salary_range, :company_id)');
-            $this->db->bind(':company_id', $_SESSION['company_id']);
+            
             $this->db->bind(':job_name', $data['job_name']);
-            $this->db->bind(':job_benifits', $data['job_benifits']);
+            $this->db->bind(':Description', $data['Description']);
             $this->db->bind(':job_location', $data['job_location']);
             $this->db->bind(':job_category', $data['job_category']);
+            $this->db->bind(':job_benifits', $data['job_benifits']);
             $this->db->bind(':adress', $data['adress']);
             $this->db->bind(':required_skills', $data['required_skills']);
             $this->db->bind(':salary_range', $data['salary_range']);
-            $this->db->bind(':Description', $data['Description']);
-
+            $this->db->bind(':company_id', $_SESSION['company_id']);
             
+
+            return $this->db->execute();
             //execute
-            if($this->db->execute()){
-                return true;
-            }else{               
-                return false;
-            }
+            // if($this->db->execute()){
+            //     return true;
+            // }else{               
+            //     return false;
+            // }
         }
 
     }
--- app/views/pages/admin/jobPost.php
+++ app/views/pages/admin/jobPost.php
@@ -4,36 +4,36 @@
 
     <div class="job-post-form-container">
         <h2>Add New Job Post</h2>
-        <form action="<?php echo URLROOT; ?>/service_provider/jobpost" method="POST" class="job-post-form">
+        <form action="<?php echo URLROOT; ?>/service_provider/jobpost" method="post" class="job-post-form">
             <div class="form-left">
                 <label for="jobName">Job Name:</label>
-                <input type="text" id="jobName" name="jobName" placeholder="Job Name" required  value="<?php echo $data['job_name']; ?>">
+                <input type="text" id="jobName" name="jobName" placeholder="Job Name" required  value="<?php  $data['job_name']; ?>">
                 <span class="form-invalid"><?php echo $data['job_name_err']; ?></span>
 
                 <label for="jobDescription">Job Description:</label>
-                <textarea id="jobDescription" name="jobDescription" placeholder="Job Description" required value="<?php echo $data['Description']; ?>"></textarea>
+                <textarea id="jobDescription" name="jobDescription" placeholder="Job Description" required value="<?php  $data['Description']; ?>"></textarea>
                 <span class="form-invalid"><?php echo $data['Description_err']; ?></span>
 
                 <label for="jobBenefits">Job Benefits:</label>
-                <input type="text" id="jobBenefits" name="jobBenefits" placeholder="Job Benefits" value="<?php echo $data['job_benifits']; ?>">
+                <input type="text" id="jobBenefits" name="jobBenefits" placeholder="Job Benefits" value="<?php  $data['job_benifits']; ?>">
                 <span class="form-invalid"><?php echo $data['job_benifits_err']; ?></span>
 
                 <label for="jobLocation">Job Location:</label>
-                <input type="text" id="jobLocation" name="jobLocation" placeholder="Job Location" value="<?php echo $data['job_location']; ?>">
+                <input type="text" id="jobLocation" name="jobLocation" placeholder="Job Location" value="<?php  $data['job_location']; ?>">
                 <span class="form-invalid"><?php echo $data['job_location_err']; ?></span>
 
                 <label for="salaryRange">Salary Range:</label>
-                <input type="text" id="salaryRange" name="salaryRange" placeholder="Salary Range" value="<?php echo $data['salary_range']; ?>">
+                <input type="text" id="salaryRange" name="salaryRange" placeholder="Salary Range" value="<?php  $data['salary_range']; ?>">
                 <span class="form-invalid"><?php echo $data['salary_range_err']; ?></span>
             </div>
 
             <div class="form-right">
                 <label for="qualifications">Required Qualifications:</label>
-                <input type="text" id="qualifications" name="qualifications" placeholder="Required Qualifications" value="<?php echo $data['required_skills']; ?>">
+                <input type="text" id="qualifications" name="qualifications" placeholder="Required Qualifications" value="<?php $data['required_skills']; ?>">
                 <span class="form-invalid"><?php echo $data['required_skills_err']; ?></span>
 
                 <label for="address">Address:</label>
-                <textarea id="address" name="address" placeholder="Address" value="<?php echo $data['adress']; ?>"></textarea>
+                <textarea id="address" name="address" placeholder="Address" value="<?php  $data['adress']; ?>"></textarea>
                 <span class="form-invalid"><?php echo $data['adress_err']; ?></span>
 
                 <label for="jobType">Job Type:</label>
