24a73a58d94f3fac0f57855c2b6530e6fda85fa9
shehara gamage <sheharagamage2002@gmail.com>
2024-11-16 13:58:25 +0530

    [UQMVP-52] create update job posting

--- app/views/pages/service_provider/edit_job.php
+++ app/views/pages/service_provider/edit_job.php
@@ -27,31 +27,31 @@
                 <div class="form-group">
                     <label for="job_title">Job Title</label>
                     <span class="arrow">-></span>
-                    <input type="text" id="job_title" name="job_title" value="<?php echo  $data['job_name']; ?>">
+                    <input type="text" id="job_title" name="jobName" value="<?php echo  $data['job_name']; ?>">
                 </div>
         
                 <div class="form-group">
                     <label for="job_description">Job Description</label>
                     <span class="arrow">-></span>
-                    <textarea id="job_description" name="job_description" ><?php echo $data['Description']; ?></textarea>
+                    <textarea id="job_description" name="jobDescription" ><?php echo $data['Description']; ?></textarea>
                 </div>
         
                 <div class="form-group">
                     <label for="job_location">Job Location</label>
                     <span class="arrow">-></span>
-                    <input type="text" id="job_location" name="job_location" value="<?php echo $data['job_location']; ?>">
+                    <input type="text" id="jobLocation" name="jobLocation" value="<?php echo $data['job_location']; ?>">
                 </div>
         
                 <div class="form-group">
                     <label for="salary_range">Salary Range</label>
                     <span class="arrow">-></span>
-                    <input type="text" id="salary_range" name="salary_range" value="<?php echo $data['salary_range']; ?>">
+                    <input type="text" id="salary_range" name="salaryRange" value="<?php echo $data['salary_range']; ?>">
                 </div>
         
                 <div class="form-group">
                     <label for="job_qualifications">Job Qualifications</label>
                     <span class="arrow">-></span>
-                    <textarea id="job_qualifications" name="job_qualifications" >
+                    <textarea id="job_qualifications" name="qualifications" >
                     <?php echo $data['required_skills']; ?>
                     </textarea>
                 </div>
@@ -59,7 +59,7 @@
                 <div class="form-group">
                     <label for="job_benefits">Job Benefits</label>
                     <span class="arrow">-></span>
-                    <textarea id="job_benefits" name="job_benefits" value="<?php echo  $data['job_benifits']; ?>">
+                    <textarea id="job_benefits" name="jobBenefits" value="<?php echo  $data['job_benifits']; ?>">
                         <?php echo  $data['job_benifits']; ?>
                     </textarea>
                 </div>
