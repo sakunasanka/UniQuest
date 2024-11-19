ba619da7809a1e2e0f14abdc1630b64bc836782d
sakunasanka <sakunasanka@gmail.com>
2024-11-20 02:36:16 +0530

    [UQMVP-68] Create deactivate popup

--- app/controllers/student.php
+++ app/controllers/student.php
@@ -275,6 +275,11 @@ class Student extends Controller
         $this->view('pages/login/wait_to_verify_stu');
     }
 
+    public function deactive()
+    {
+        $this->view('pages/login/deactivate_stu');
+    }
+
     public function internships()
     {
         $this->view('pages/student/internships');
