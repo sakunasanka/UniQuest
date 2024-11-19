422cc3b00781aa8a00ec944a234fd08e5f85130d
sakunasanka <sakunasanka@gmail.com>
2024-11-19 20:46:47 +0530

    [UQMVP-63] Change config.php to hosted db and place uploads in .gitignore

--- app/config/config.php
+++ app/config/config.php
@@ -15,10 +15,10 @@
     define('SITENAME', 'UniQuest');
 
     //DB Params
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_NAME', 'uniquest');
+    define('DB_HOST', 'mysql-uniquestdb.alwaysdata.net');  //mysql-uniquestdb.alwaysdata.net
+    define('DB_USER', '385988_sakuna');  //385988_sakuna
+    define('DB_PASS', 'sakuna_1'); //sakuna_1
+    define('DB_NAME', 'uniquestdb_uniquest');  //uniquestdb_uniquest
 
     // Enable error displaying (for development only, consider disabling in production)
     ini_set('display_errors', 1);
