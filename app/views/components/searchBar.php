a17befc6a9b2cf9b048c20bd1b27544383662a0b
sakiththewmika <sakiththewmika@gmail.com>
2024-11-28 00:52:19 +0530

    [UQMVP-35] Implement search bar

---
+++ app/views/components/searchBar.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/components/searchBar.css">
+
+
+<div class="content-header">
+    <div class="search-bar">
+        <span class="material-symbols-outlined icon">search</span>
+        <input type="text" class="search" placeholder="Search for Jobs, Internships and Companies">
+        <select class="column-select">
+            <option value="1">Name</option>
+            <option value="2">Rating</option>
+            <option value="3">Location</option>
+        </select>
+
+    </div>
+</div>
