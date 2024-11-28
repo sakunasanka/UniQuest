0a89426bc37bfbb310f09b3a7549f5b1a7f45339
sakunasanka <sakunasanka@gmail.com>
2024-11-28 19:09:02 +0530

    [UQMVP-64] Recreate not found pages

---
+++ app/views/pages/not_found/403_not_found.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/pages/404_not_found/page_not_found.css">
+
+<div class="container">
+    <h1>403</h1>
+    <h2>Forbidden</h2>
+    <p>We are sorry, but you do not have access to this page or resource.</p>
+    <a href="/uniquest/home" class="home-button">Go to Home Page</a>
+</div>
