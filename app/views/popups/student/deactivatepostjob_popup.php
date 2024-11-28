37df8d6b51070123198f27af31e4d646f83ac320
shehara gamage <sheharagamage2002@gmail.com>
2024-11-28 15:37:14 +0530

    [UQMVP-65] update delete popup

--- app/views/popups/student/deactivatepostjob_popup.php
+++ app/views/popups/student/deactivatepostjob_popup.php
@@ -5,21 +5,21 @@
         <div class="overlay"></div>
         <div class="content">
         <div class="close-btn-container"><button class="close-btn" onclick="closedeletereviewconfirm()"><i class="fa fa-times"></i></button></div>
-            <h2>Delete review</h2>
+            <h2>Delete post</h2>
             <input type="hidden" id="postID">
             <form action="<?php echo URLROOT; ?>/service_provider/delete" id="delete-post-form" method="POST">
                 <input type="hidden" id="postID">
                 <div class="warning">
-                    <p>Are you sure you want to delete your review?<br>
-                        Once you delete your review, you can't recover it.</p>
+                    <p>Are you sure you want to delete your post?<br>
+                        Once you delete your post, you can't recover it.</p>
                 </div>
                 <label>
                     <input type="checkbox" name="confirm" value="yes" required>
-                    <div class="label-text">I confirm my review deletion</div>
+                    <div class="label-text">I confirm my post deletion</div>
                 </label>
                 <div class="buttons">
                     <a onclick="canceldeletereviewconfirm()" href="#" class="cancel-btn">Cancel</a>
-                  <button type="submit" class="delete-btn">Delete review</button>
+                  <button type="submit" class="delete-btn">Delete post</button>
                 </div>
             </form>
         </div>
