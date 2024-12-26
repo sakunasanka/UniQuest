d0c034d60b0aa8ea5c3e8c80dd203bbae9c3bc85
Wameesha <rasanjani9jayasingha@gmail.com>
2024-12-26 15:11:39 +0530

    [UQMVP-72] Add correct path to ver_team contact_admin page

--- app/views/components/ver_header.php
+++ app/views/components/ver_header.php
@@ -31,7 +31,7 @@
                 </li>
                 <li><a href="/uniquest/verification_team/user_ver_all" class="hov">Go to verify</a></li>
                 <!-- <li><a href="/uniquest/about" class="hov">About Us</a></li> -->
-                <li><a href="/uniquest/service_provider/contact_admin" class="hov">Contact Us</a></li>
+                <li><a href="/uniquest/verification_team/contact_admin" class="hov">Contact Us</a></li>
             </ul>
             <?php require APPROOT . '/views/components/navProfile.php'; ?>
             <div class="nav-icons">
