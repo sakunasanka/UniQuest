d20ed9e186a560e2ea529d5d64cfcaf12400acae
sakiththewmika <sakiththewmika@gmail.com>
2025-02-13 23:34:06 +0530

    [UQMVP-79] Implement university email validator

--- app/bootloader.php
+++ app/bootloader.php
@@ -19,6 +19,7 @@ require_once 'helpers/Sorter.php';
 require_once 'helpers/MailHelper.php';
 require_once 'helpers/LogHelper.php';
 require_once 'helpers/TokenHelper.php';
+require_once 'helpers/UniversityEmailValidator.php';
 
 
 //load middlewares
