0e43ee440b002324235dd6d494973c275c08b345
Wameesha <rasanjani9jayasingha@gmail.com>
2025-04-07 23:31:58 +0530

    [UQMVP-72] Edit preview button of message_com

--- app/views/pages/admin/messages_com.php
+++ app/views/pages/admin/messages_com.php
@@ -40,9 +40,18 @@
                                 <td><?php echo $message->created_at ?></td>
                                 <td><span class="status active"><?php echo $message->read_status ?></span></td>
                                 <td class="action">
-                                    <button id="openPopupBtn" class="open-btn-2 material-symbols-outlined action-btn view">
+                                <?php if ($message->sender_role == 'Company') : ?>
+                                    <button id="openPopupBtn" class="open-btn-2 material-symbols-outlined action-btn view" 
+                                        onclick="window.location.href='<?php echo URLROOT; ?>/admin/messages_com/<?php echo $message->sender_id; ?>'">
                                         preview
                                     </button>
+                                    <?php elseif ($message->receiver_role == 'Student'):?>
+                                        <button id="openPopupBtn" class="open-btn-2 material-symbols-outlined action-btn view" 
+                                        onclick="window.location.href='<?php echo URLROOT; ?>/admin/messages_com/<?php echo $message->receiver_id; ?>'">
+                                        preview
+                                    </button>
+                                <?php endif; ?>
+
                                 </td>
                             </tr>
                         <?php endforeach; ?>
@@ -53,7 +62,7 @@
                     <?php endif; ?>
                 </tbody>
             </table>
-            <?php require APPROOT . '/views/components/pagination.php'; ?>
+            <?php //require APPROOT . '/views/components/pagination.php'; ?>
         </div>
     </main>
 </div>
--- app/views/pages/admin/messages_ver.php
+++ app/views/pages/admin/messages_ver.php
@@ -40,9 +40,17 @@
                                 <td><?php echo $message->created_at ?></td>
                                 <td><span class="status active"><?php echo $message->read_status ?></span></td>
                                 <td class="action">
-                                    <button id="openPopupBtn" class="open-btn-2 material-symbols-outlined action-btn view">
+                                <?php if ($message->sender_role == 'VT-Member') : ?>
+                                    <button id="openPopupBtn" class="open-btn-2 material-symbols-outlined action-btn view" 
+                                        onclick="window.location.href='<?php echo URLROOT; ?>/admin/messages_ver/<?php echo $message->sender_id; ?>'">
                                         preview
                                     </button>
+                                    <?php elseif ($message->receiver_role == 'VT-Member'):?>
+                                        <button id="openPopupBtn" class="open-btn-2 material-symbols-outlined action-btn view" 
+                                        onclick="window.location.href='<?php echo URLROOT; ?>/admin/messages_ver/<?php echo $message->receiver_id; ?>'">
+                                        preview
+                                    </button>
+                                <?php endif; ?>
                                 </td>
                             </tr>
                         <?php endforeach; ?>
@@ -53,7 +61,7 @@
                     <?php endif; ?>
                 </tbody>
             </table>
-            <?php require APPROOT . '/views/components/pagination.php'; ?>
+            <?php //require APPROOT . '/views/components/pagination.php'; ?>
         </div>
     </main>
 </div>
