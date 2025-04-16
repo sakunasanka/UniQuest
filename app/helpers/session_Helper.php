a4ff788f64497857cc4f7e77fa0c2725074ea3c9
sakunasanka <sakunasanka@gmail.com>
2025-04-17 03:31:20 +0530

    [UQMVP-86] Seperate the errors as part time and internship error when reapplying to a jobpost

--- app/helpers/session_Helper.php
+++ app/helpers/session_Helper.php
@@ -28,11 +28,20 @@ function flash($name) {
 <?php unset($_SESSION['show_report_error']); ?>
 <?php endif; ?>
 
-<?php if(isset($_SESSION['show_apply_error'])): ?>
+<?php if(isset($_SESSION['show_job_apply_error'])): ?>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         Flash.show("You have already applied for this job", "error");
     });
 </script>
-<?php unset($_SESSION['show_apply_error']); ?>
+<?php unset($_SESSION['show_job_apply_error']); ?>
+<?php endif; ?>
+
+<?php if(isset($_SESSION['show_internship_apply_error'])): ?>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        Flash.show("You have already applied for this internship", "error");
+    });
+</script>
+<?php unset($_SESSION['show_internship_apply_error']); ?>
 <?php endif; ?>
