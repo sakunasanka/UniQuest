d1f1bb87a73ce1c1eba62a4b2b8536c947d7b798
sakunasanka <sakunasanka@gmail.com>
2025-04-20 12:59:41 +0530

    [UQMVP-90] Create helper function for accept student application

--- app/helpers/NotificationHelper.php
+++ app/helpers/NotificationHelper.php
@@ -4,3 +4,18 @@ function sendNotification($userId, $message, $type = 'info', $link = null) {
     $notificationModel = new NotificationModel();
     return $notificationModel->create($userId, $type, substr($message, 0, 100), $message, $link);
 }
+
+function notifyStudentApplicationAccepted($applicationId, $studentId, $jobTitle) {
+    $notificationModel = new NotificationModel();
+    
+    $message = "Congratulations! Your application for '{$jobTitle}' has been accepted";
+    $link = "/student/applications/view/{$applicationId}";
+    
+    return $notificationModel->create(
+        $studentId,
+        'success', // notification type
+        'Application Accepted', // title
+        $message,
+        $link
+    );
+}
--- app/views/components/header.php
+++ app/views/components/header.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo SITENAME ?></title>
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/components/navbar.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/components/notifications.css">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="<?php echo URLROOT; ?>/js/components/flash.js"></script>
+    <script src="<?php echo URLROOT; ?>/js/components/notifications.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
 
     <?php if (isset($_SESSION['user_role'])): ?>
