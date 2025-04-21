ddea4311b65e67cf68834eb1e80e43c3bc331e66
sakunasanka <sakunasanka@gmail.com>
2025-04-21 14:10:38 +0530

    Resolve conflicts between UQMVP-72 and dev

--- app/views/pages/verification_team/contact_admin.php
+++ app/views/pages/verification_team/contact_admin.php
@@ -1,5 +1,4 @@
 <?php require APPROOT . '/views/components/header.php'; ?>
-<?php require APPROOT . '/views/components/ser_header.php'; ?>
 
 <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/components/chat.css">
 <div class="main-container">
