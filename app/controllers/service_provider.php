1881df92433f846e679cccbd6d1e5741f81713f4
sakunasanka <sakunasanka@gmail.com>
2025-04-23 02:58:12 +0530

    [UQMVP-93] Write functions for limit application count and check can post flag

--- app/controllers/service_provider.php
+++ app/controllers/service_provider.php
@@ -933,13 +933,20 @@ class Service_provider extends Controller
         $companyPosts = $this->model('M_jobpost')->getJobsByCompanyId($_SESSION['user_id']);
         $companyInfo = $this->model('M_jobpost')->getpostbycompanyid($_SESSION['user_id']);
         $NewPostCount = count($companyPosts);
+        $can_post = $this->model('M_jobpost')->canPostJob();
 
         //Current date - subscription end date
         $currentDate = date('Y-m-d');
         $subscriptionEndDate = $companyInfo->subscription_end_date;
         $remainingDays = waitForTime(strtotime($subscriptionEndDate) - strtotime($currentDate));
 
-        if ($companyInfo->subscription_plan == 'free' && $NewPostCount >= 2) {
+        if($can_post == 'N') {
+            $_SESSION['show_canpost_job_error'] = true;
+            $previousURL = $_SERVER['HTTP_REFERER'] ?? URLROOT . '/service_provider/dashboard';
+            Redirect::to($previousURL);
+        }
+
+        elseif ($companyInfo->subscription_plan == 'free' && $NewPostCount >= 2) {
             $_SESSION['show_job_post_error_free'] = true;
             $_SESSION['remaining_days'] = $remainingDays;
             $previousURL = $_SERVER['HTTP_REFERER'] ?? URLROOT . '/service_provider/premium';
