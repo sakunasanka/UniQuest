3b75afa4c847730d973af38e855cf5793948d1c1
shehara gamage <sheharagamage2002@gmail.com>
2025-04-24 17:03:37 +0530

    [UQMVP-92-copy] Change db password

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
     //DB Params
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', '1234');
+    define('DB_PASS', '');
     define('DB_NAME', 'uniquest');
     
     //App Root
